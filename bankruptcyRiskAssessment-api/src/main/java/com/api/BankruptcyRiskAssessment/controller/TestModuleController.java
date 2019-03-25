5c757d8d8cfe339a999f342fa32d9ae61bb480d9
AlexanderNovosad <nztux@ukr.net>
2019-03-25 19:25:13 +0200

    Correct exceptions and request parameters.

--- bankruptcyRiskAssessment-api/src/main/java/com/api/BankruptcyRiskAssessment/controller/TestModuleController.java
+++ bankruptcyRiskAssessment-api/src/main/java/com/api/BankruptcyRiskAssessment/controller/TestModuleController.java
@@ -18,20 +18,23 @@ public class TestModuleController {
     /*Test Category*/
     @PostMapping("/testCategory")
     public ResponseEntity<TestCategory> addTestCategory(@RequestBody TestCategory testCategory){
+        if(testCategory==null){
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok(testModuleService.addTestCategory(testCategory));
     }
 
     @DeleteMapping("/testCategory")
-    public ResponseEntity<TestCategory> deleteTestCategory(Long testCategoryId){
+    public ResponseEntity<TestCategory> deleteTestCategory(@RequestParam(value = "testCategory_id") Long testCategoryId){
         TestCategory testCategory = testModuleService.deleteTestCategory(testCategoryId);
         if(testCategory==null){
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(testCategory);
     }
 
     @GetMapping("/testCategory")
-    public ResponseEntity<TestCategory> getTestCategory(Long testCategoryId){
+    public ResponseEntity<TestCategory> getTestCategory(@RequestParam(value = "testCategory_id") Long testCategoryId){
         TestCategory testCategory = testModuleService.getTestCategory(testCategoryId);
         if(testCategory==null){
             return ResponseEntity.notFound().build();
@@ -40,33 +43,42 @@ public class TestModuleController {
     }
 
     @PutMapping("/testCategory")
-    public ResponseEntity<TestCategory> updateTestCategory(TestCategory testCategory){
+    public ResponseEntity<TestCategory> updateTestCategory(@RequestBody TestCategory testCategory){
+        if(testCategory==null){
+            return ResponseEntity.badRequest().build();
+        }
         testModuleService.updateTestCategory(testCategory);
         return ResponseEntity.ok(testCategory);
     }
 
     @GetMapping("/testCategories")
     public ResponseEntity<List<TestCategory>> getAllTestCategories(){
+        if (testModuleService.getAllTestCategories() == null){
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok(testModuleService.getAllTestCategories());
     }
 
     /*Test*/
     @PostMapping("/test")
-    public ResponseEntity<Test> addTest(Test test){
+    public ResponseEntity<Test> addTest(@RequestBody Test test){
+        if(test == null){
+            return ResponseEntity.badRequest().build();
+        }
         return ResponseEntity.ok(testModuleService.addTest(test));
     }
 
     @DeleteMapping("/test")
-    public ResponseEntity<Test> deleteTest(Long testId){
+    public ResponseEntity<Test> deleteTest(@RequestParam(value = "test_id") Long testId){
         Test test = testModuleService.deleteTest(testId);
         if(test==null){
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(test);
     }
 
     @GetMapping("/test")
-    public ResponseEntity<Test> getTest(Long testId){
+    public ResponseEntity<Test> getTest(@RequestParam(value = "test_id") Long testId){
         Test test = testModuleService.getTest(testId);
         if(test==null){
             return ResponseEntity.notFound().build();
@@ -80,7 +92,7 @@ public class TestModuleController {
     }
 
     @GetMapping("/testCategory/tests")
-    public ResponseEntity<List<Test>> findTestsByTestCategory(Long testCategoryId){
+    public ResponseEntity<List<Test>> findTestsByTestCategory(@RequestParam(value = "testCategory_id") Long testCategoryId){
         List<Test> tests = testModuleService.findTestsByTestCategory(testCategoryId);
         if (tests==null){
             return ResponseEntity.notFound().build();
@@ -90,30 +102,33 @@ public class TestModuleController {
 
     /*Questions for test*/
     @PostMapping("/question")
-    public ResponseEntity<Question> addQuestion(Question question){
+    public ResponseEntity<Question> addQuestion(@RequestBody Question question){
+        if(question == null){
+            ResponseEntity.badRequest().build();
+        }
         return ResponseEntity.ok(testModuleService.addQuestion(question));
     }
 
     @DeleteMapping("/question")
-    public ResponseEntity<Question> deleteQuestion(Long questionId){
+    public ResponseEntity<Question> deleteQuestion(@RequestParam(value = "question_id") Long questionId){
         Question question = testModuleService.deleteQuestion(questionId);
         if(question==null){
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(question);
     }
 
     @DeleteMapping("/test/questions")
-    public ResponseEntity<List<Question>> deleteQuestionsByTest(Long testId){
+    public ResponseEntity<List<Question>> deleteQuestionsByTest(@RequestParam(value = "test_id") Long testId){
         List<Question> questions = testModuleService.deleteQuestionsByTest(testId);
         if(questions==null){
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(questions);
     }
 
     @GetMapping("/test/questions")
-    public ResponseEntity<List<Question>> findQuestionsByTest(Long testId){
+    public ResponseEntity<List<Question>> findQuestionsByTest(@RequestParam(value = "test_id") Long testId){
         List<Question> questions = testModuleService.findQuestionsByTest(testId);
         if(questions==null){
             return ResponseEntity.notFound().build();
@@ -122,46 +137,52 @@ public class TestModuleController {
     }
 
     @PutMapping("/question")
-    public ResponseEntity<Question> updateQuestion(Question question){
+    public ResponseEntity<Question> updateQuestion(@RequestBody Question question){
+        if(question==null){
+            return ResponseEntity.badRequest().build();
+        }
         testModuleService.updateQuestion(question);
         return ResponseEntity.ok(question);
     }
 
     /*Answers for questions*/
     @PostMapping("/answer")
-    public ResponseEntity<Answer> addAnswer(Answer answer){
+    public ResponseEntity<Answer> addAnswer(@RequestBody Answer answer){
+        if(answer == null){
+            return ResponseEntity.badRequest().build();
+        }
         return ResponseEntity.ok(testModuleService.addAnswer(answer));
     }
 
     @DeleteMapping("user/test/answers")
-    public ResponseEntity<List<Answer>> deleteAnswersByTest(Long testId, Long userId){
+    public ResponseEntity<List<Answer>> deleteAnswersByTest(@RequestParam(value = "test_id") Long testId, @RequestParam(value = "user_id") Long userId){
         List<Answer> answers = testModuleService.deleteAnswersByTest(testId, userId);
         if (answers==null){
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(answers);
     }
 
     @DeleteMapping("/test/answers")
-    public ResponseEntity<List<Answer>> deleteAnswersByTest(Long testId){
+    public ResponseEntity<List<Answer>> deleteAnswersByTest(@RequestParam(value = "test_id") Long testId){
         List<Answer> answers = testModuleService.deleteAnswersByTest(testId);
         if (answers==null){
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(answers);
     }
 
     @DeleteMapping("/question/answers")
-    public ResponseEntity<List<Answer>> deleteAnswersByQuestion(Long questionId){
+    public ResponseEntity<List<Answer>> deleteAnswersByQuestion(@RequestParam(value = "question_id") Long questionId){
         List<Answer> answers = testModuleService.deleteAnswersByQuestion(questionId);
         if (answers==null){
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(answers);
     }
 
     @GetMapping("user/test/answers")
-    public ResponseEntity<List<Answer>> findAllAnswersForTest(Long testId, Long userId){
+    public ResponseEntity<List<Answer>> findAllAnswersForTest(@RequestParam(value = "test_id") Long testId, @RequestParam(value = "user_id") Long userId){
         List<Answer> answers = testModuleService.findAllAnswersForTest(testId, userId);
         if (answers==null){
             return ResponseEntity.notFound().build();
@@ -170,45 +191,48 @@ public class TestModuleController {
     }
 
     @PutMapping("/answer")
-    public ResponseEntity<Answer> confirmAnswer(Answer answer){
+    public ResponseEntity<Answer> confirmAnswer(@RequestBody Answer answer){
+        if(answer == null){
+            return ResponseEntity.badRequest().build();
+        }
         testModuleService.confirmAnswer(answer);
         return ResponseEntity.ok(answer);
     }
 
     /*TestResult*/
     @DeleteMapping("/tset/testResult")
-    public ResponseEntity<List<TestResult>> deleteTestResultByTest(Long testId){
+    public ResponseEntity<List<TestResult>> deleteTestResultByTest(@RequestParam(value = "test_id") Long testId){
         List<TestResult> testResults = testModuleService.deleteTestResultByTest(testId);
         if(testResults==null){
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(testResults);
     }
 
     /*Calculations*/
     @PostMapping("user/test/testResult")
-    public ResponseEntity<TestResult> calculateTestPointsForUser(Long testId, Long userId){
+    public ResponseEntity<TestResult> calculateTestPointsForUser(@RequestParam(value = "test_id") Long testId, @RequestParam(value = "user_id") Long userId){
         TestResult testResult = testModuleService.calculateTestPointsForUser(testId, userId);
         if(testResult==null){
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(testResult);
     }
 
     @PostMapping("user/userEfficiencyFactory")
-    public ResponseEntity<List<UserEfficiencyFactor>> calculateUserEfficiencyFactory(Long userId){
+    public ResponseEntity<List<UserEfficiencyFactor>> calculateUserEfficiencyFactory(@RequestParam(value = "user_id") Long userId){
         List<UserEfficiencyFactor> userEfficiencyFactors = testModuleService.calculateUserEfficiencyFactory(userId);
         if(userEfficiencyFactors==null){
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(userEfficiencyFactors);
     }
 
     @PostMapping("department/departmentEfficiencyFactory")
-    public ResponseEntity<DepartmentEfficiencyFactor> calculateDepartmentEfficiencyFactory(Long departmentId){
+    public ResponseEntity<DepartmentEfficiencyFactor> calculateDepartmentEfficiencyFactory(@RequestParam(value = "department_id") Long departmentId){
         DepartmentEfficiencyFactor departmentEfficiencyFactor = testModuleService.calculateDepartmentEfficiencyFactory(departmentId);
         if(departmentEfficiencyFactor==null){
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(departmentEfficiencyFactor);
     }
